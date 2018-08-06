5e1a425d99c45243728550c02a44f01a64c12ad5
Khoa Huynh <khoawin92@gmail.com>
2018-08-05 20:39:29 -0700

    hotfix for heroku refresh

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 8888;
 app.use(express.static('./build'));
-app.get('*', (req, res) => res.sendFile(path.resolve('build', `${__dirname}/src/index.html`)));
+app.get('*', (req, res) => res.sendFile(path.resolve('build', `index.html`)));
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
