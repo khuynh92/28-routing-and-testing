2d28dbd0ebc1d7bc26fdd3609d92f1b05bd29e5f
Khoa Huynh <khoawin92@gmail.com>
2018-08-05 20:35:56 -0700

    hotfix for heroku refresh

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 8888;
 app.use(express.static('./build'));
 app.get('*', (req, res) => res.sendFile(path.resolve('build', `${__dirname}/src/index.html`)));
